368f1e8b7b65d1b41cd2b07664093ed1a0bbd9a9
Guilherme Lima Santos <grguilh2rm2lima@gmail.com>
2024-09-26 03:05:52 -0300

    Adicionada View Model de Manter Produto, #75.

---
+++ Codigo/CuidadoWeb/CuidadoWeb/Models/ProdutoViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CuidadoWeb.Models
+{
+	public class ProdutoViewModel
+	{
+		[Key]
+		public uint Id { get; set; }
+
+		[Required(ErrorMessage = "Campo requerido")]
+		[StringLength(50, MinimumLength = 5, ErrorMessage = "Nome do produto deve ter entre 5 e 45 caracteres")]
+		public string Nome { get; set; } = null!;
+
+		[Required(ErrorMessage = "Campo requerido")]
+		[Display(Name = "Classificação")]
+		[StringLength(30, MinimumLength = 1, ErrorMessage = "Classificação do produto deve ter entre 1 e 30 caracteres")]
+		public string Classificacao { get; set; } = null!;
+
+		[ForeignKey("Organizacao")]
+		public int IdOrganizacao { get; set; }
+	}
+}
