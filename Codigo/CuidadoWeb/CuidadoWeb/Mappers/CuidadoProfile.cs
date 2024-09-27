3f802ed0ceaab93591127d7192ba83004028aaaa
Jonatha Gabriel <jonathagabrielns@gmail.com>
2024-09-26 23:56:31 -0300

    implentação do controller do cuidado

---
+++ Codigo/CuidadoWeb/CuidadoWeb/Mappers/CuidadoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Core;
+using CuidadoWeb.Models;
+
+namespace CuidadoWeb.Mappers
+{
+	public class CuidadoProfile : Profile
+	{
+		public CuidadoProfile()
+		{
+			CreateMap<CuidadoViewModel, Cuidado>().ReverseMap();
+		}
+	}
+}
