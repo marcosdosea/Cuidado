e7db2d2e017b3741e39d88207c45a5c6b4654eb1
Fabio <fcdiniz07@gmail.com>
2024-09-27 15:06:21 -0300

    Adicionado Mapper de Manter Residente #63

---
+++ Codigo/CuidadoWeb/CuidadoWeb/Mappers/ResidenteProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using CuidadoWeb.Models;
+
+namespace CuidadoWeb.Mappers
+{
+	public class ResidenteProfile : Profile
+	{
+		public ResidenteProfile() 
+		{
+			CreateMap<ResidenteViewModel, ResidenteViewModel>().ReverseMap();
+		}
+	}
+}
