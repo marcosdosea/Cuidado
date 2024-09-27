4a9f1deeb42b04d73cce525496a022ec612fb8d2
Igor Andrade Nepomuceno <igor.menino23@gmail.com>
2024-09-26 21:53:52 -0300

    Implementação da Controller Do manter visitante

--- Codigo/CuidadoWeb/CuidadoWeb/Program.cs
+++ Codigo/CuidadoWeb/CuidadoWeb/Program.cs
@@ -5,49 +5,49 @@ using Service;
 
 namespace CuidadoWeb
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			var builder = WebApplication.CreateBuilder(args);
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-			// Add services to the container.
-			builder.Services.AddControllersWithViews();
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
 
-			builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-			var connectionString = builder.Configuration.GetConnectionString("CuidadoDatabase") ?? throw new InvalidOperationException(); ;
-			builder.Services.AddDbContext<CuidadoContext>(options =>
-			{
-				options.UseMySQL(connectionString);
-			});
+            var connectionString = builder.Configuration.GetConnectionString("CuidadoDatabase") ?? throw new InvalidOperationException(); ;
+            builder.Services.AddDbContext<CuidadoContext>(options =>
+            {
+                options.UseMySQL(connectionString);
+            });
 
-			builder.Services.AddTransient<IProdutoService, ProdutoService>();
-			builder.Services.AddTransient<IFuncionarioService, FuncionarioService>();
+            builder.Services.AddTransient<IProdutoService, ProdutoService>();
+            builder.Services.AddTransient<IFuncionarioService, FuncionarioService>();
             builder.Services.AddTransient<IVisitanteService, VisitanteService>();
 
             var app = builder.Build();
 
-			// Configure the HTTP request pipeline.
-			if (!app.Environment.IsDevelopment())
-			{
-				app.UseExceptionHandler("/Home/Error");
-				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-				app.UseHsts();
-			}
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-			app.UseHttpsRedirection();
-			app.UseStaticFiles();
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
 
-			app.UseRouting();
+            app.UseRouting();
 
-			app.UseAuthorization();
+            app.UseAuthorization();
 
-			app.MapControllerRoute(
-				name: "default",
-				pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
 
-			app.Run();
-		}
-	}
+            app.Run();
+        }
+    }
 }
